8628a839ca958505a608f5d23fc79d7658b2c782
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-28 16:09:39 +0200

    add functionality to send the form

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburger = document.getElementById("hamburger-icon");
 const navBarContainer = document.querySelector(".nav-bar__container");
 const formLabels = document.querySelectorAll(".contact__form label");
+const messageSubmitBtn = document.getElementById("send-message");
+const contactFrom = document.getElementById("contact-form");
 
 hamburger.addEventListener("click", () => {
   if (navBarContainer.classList.contains("menu-open")) {
@@ -19,3 +21,32 @@ formLabels.forEach((label) => {
     })
     .join("");
 });
+
+messageSubmitBtn.addEventListener("click", sendMessage);
+
+async function sendMessage(e) {
+  e.preventDefault();
+
+  const formData = new FormData(contactFrom);
+
+  const response = await fetch(
+    "https://ancient-reef-77698-e8323d8efe20.herokuapp.com/api/v1/messages",
+    {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
+
+  const data = await response.json();
+
+  if (data.success) {
+    alert("Message sent successfully");
+    contactFrom.reset();
+    return;
+  }
+}
